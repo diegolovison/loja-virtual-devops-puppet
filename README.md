34b700e5918547324d1e024d64730d0ff27c4704
Diego Lovison <diegolovison@gmail.com>
2014-07-07 21:54:32 -0300

    format c:

--- README.md
+++
@@ -1,4 +0,0 @@
-Loja Virtual de Demonstração (Puppet)
-=====================================
-
-Repositório com código puppet exemplo para configuração e deploy da loja virtual.
--- modules/mysql/spec/classes/client_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-
-describe 'mysql::client' do
-  it {
-    should contain_package('mysql-client').with_ensure('installed')
-  }
-end
